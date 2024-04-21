b9c19a2de5dc0d2ab13195d709d105f1e85f4e1e
Nika <bogomolova.veronika101@gmail.com>
2024-04-21 08:12:01 +0500

    add localization

--- lib/app/ui/main_app.dart
+++ lib/app/ui/main_app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:max_app/app/value/colors/app_colors.dart';
 import 'package:max_app/app/value/theme/app_text_theme.dart';
 import 'package:max_app/app/view/main_view.dart';
+import 'package:max_app/generated/l10n.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -9,6 +11,15 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: S.delegate.supportedLocales
+          .firstWhere((element) => element.languageCode == 'ru'),
       title: 'Maximal App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
