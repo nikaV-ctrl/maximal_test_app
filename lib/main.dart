ab8f85ef4cd71982ef337035d9072128585e4f70
Nika <bogomolova.veronika101@gmail.com>
2024-04-22 20:03:25 +0500

    add di

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:max_app/app/di/init_di.dart';
 import 'package:max_app/app/ui/main_app.dart';
 
 void main() {
   runApp(const MainApp());
+  initDi();
 }
